a289c1bd62ad5f252b4ea59be498ca53cd1dce75
Ziad Gad <ziadamrjad@gmail.com>
2025-04-22 02:26:06 +0200

    prepare for vercel hosting

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const DB =
-  process.env.NODE_ENV === "development" ? process.env.LOCAL_DATABASE : "";
+  process.env.NODE_ENV === "development"
+    ? process.env.LOCAL_DATABASE
+    : `${process.env.DATABASE}`;
 const dbConnection = () => {
   mongoose.connect(DB).then(() => {
-    console.log("DB Connected Successful!");
+    console.log(`DB Connected in ${process.env.NODE_ENV} Env Successful!`);
   });
 };
 
